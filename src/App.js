e86fb1fab45709567a05e6554c4e1a5e0b0f1d50
Akash1507 <atakashsharma@gmail.com>
2020-01-10 20:10:50 +0530

    import custom layout and article list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./App.css";
 import "antd/dist/antd.css";
-
+import ArticleList from "./containers/ArticleListView";
+import CustomLayout from "./containers/Layout";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CustomLayout>
+        <ArticleList />
+      </CustomLayout>
+    </div>
+  );
 }
 
 export default App;
