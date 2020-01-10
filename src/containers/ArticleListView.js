80371dabaf4a4a0175cdd5238f3bd845d7eb67c0
Akash1507 <atakashsharma@gmail.com>
2020-01-11 02:40:56 +0530

    add form for creating of article

--- src/containers/ArticleListView.js
+++ src/containers/ArticleListView.js
@@ -1,9 +1,29 @@
 import React from "react";
-import Article from "../components/Article";
+import Articles from "../components/Article";
+import CustomForm from "../components/Form";
+import axios from "axios";
 
 class ArticleList extends React.Component {
+  state = {
+    articles: []
+  };
+
+  componentDidMount() {
+    axios.get("http://127.0.0.1:8000/api/").then(res => {
+      this.setState({
+        articles: res.data
+      });
+      console.log(res.data);
+    });
+  }
   render() {
-    return <Article />;
+    return (
+      <div>
+        <Articles data={this.state.articles} />
+        <h2>Create an Article</h2>
+        <CustomForm requestType="post" articleId="null" btnText="Create" />
+      </div>
+    );
   }
 }
 
