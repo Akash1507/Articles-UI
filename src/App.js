5159a2d7f576810d5587a759f803af8fa71e3614
Akash1507 <atakashsharma@gmail.com>
2020-01-12 12:14:10 +0530

    make app component class based and use redux connect method

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from "react";
+import { connect } from "react-redux";
 import "antd/dist/antd.css";
 import CustomLayout from "./containers/Layout";
 import { BrowserRouter as Router } from "react-router-dom";
 import BaseRouter from "./routes";
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <CustomLayout>
-          <BaseRouter />
-        </CustomLayout>
-      </div>
-    </Router>
-  );
+import * as actions from "./store/actions/auth";
+class App extends React.Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <CustomLayout {...this.props}>
+            <BaseRouter />
+          </CustomLayout>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.token !== null
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(App);
