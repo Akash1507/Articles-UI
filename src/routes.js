dde6f1e9156d0490f8bedbb02b676d4479d32217
Akash1507 <atakashsharma@gmail.com>
2020-01-12 12:18:09 +0530

    add login,signup url

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Route } from "react-router-dom";
 import ArticleList from "./containers/ArticleListView";
 import ArticleDetail from "./containers/ArticleDetailView";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 
 const BaseRouter = () => (
   <div>
     <Route exact path="/" component={ArticleList} />
-    <Route path="/:articleId" component={ArticleDetail} />
+    <Route path="/articles/articleId" component={ArticleDetail} />
+    <Route path="/login/" component={Login} />
+    <Route path="/signup/" component={Signup} />
   </div>
 );
 
